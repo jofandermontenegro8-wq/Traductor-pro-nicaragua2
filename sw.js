4a4e1d0ecf582d02d4cd60ec932059a29598cdf6
Jofander Montenegro <jofandermontenegro8@gmail.com>
2026-02-26 21:20:27 -0600

    Crear sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const cacheName = 'traductor-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Screenshot_20260226-205258.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
